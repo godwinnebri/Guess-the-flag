3fb0439aa8d4e71505f8cd8b122bc395892057ce
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-09-15 14:13:56 +0100

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Godwin IE on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
